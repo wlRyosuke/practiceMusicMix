a60a42fbbd146108addb698b2471c617e5f74c5f
Kai Rodriguez <ah1511579@gmail.com>
2017-08-10 14:17:38 +0900

    Initial Commitテスト

---
+++ Musicmix/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Musicmix
+//
+//  Created by ロドリゲス海 on 2017/08/10.
+//  Copyright © 2017年 ロドリゲス海. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Musicmix/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Musicmix
+//
+//  Created by ロドリゲス海 on 2017/08/10.
+//  Copyright © 2017年 ロドリゲス海. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
