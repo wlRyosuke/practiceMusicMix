4da0c0073fe480ee858848aa86854e89d3cbc1a1
Kai Rodriguez <ah1511579@gmail.com>
2017-08-10 15:51:33 +0900

    AFNetworkのインストール

---
+++ Pods/Target Support Files/Pods-Musicmix/Pods-Musicmix-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Musicmix : NSObject
+@end
+@implementation PodsDummy_Pods_Musicmix
+@end
---
+++ Pods/Target Support Files/Pods-MusicmixUITests/Pods-MusicmixUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MusicmixUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MusicmixUITests
+@end
